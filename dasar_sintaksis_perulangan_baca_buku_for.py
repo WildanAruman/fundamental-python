3a4a79e86f7b7b06a00ff53430b1581e8f310e6c
WildanAruman <wildan.aruman90@gmail.com>
2022-03-12 13:45:11 +0700

    perulangan dengan for dan while untuk jumlah buku tertentu, contoh = 10

---
+++ dasar_sintaksis_perulangan_baca_buku_for.py
@@ -0,0 +1,15 @@
+"""
+Program perulangan membaca buku
+"""
+Jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu!')
+
+jumlah_buku_dibaca = 0
+
+for jumlah_buku_dibaca in range(1, Jumlah_buku+1):
+    print(f'buku yang di baca ke {jumlah_buku_dibaca} sudah dibaca')
+
+
+    print(f'jumlah buku yang sudah dibaca {jumlah_buku_dibaca} ')
+
+
---
+++ dasar_sintaksis_perulangan_baca_buku_while.py
@@ -0,0 +1,17 @@
+"""
+Program perulangan baca buku dengan while
+
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu!')
+
+jumlah_buku_dibaca = 0
+
+print(f'Buku yang sudah dibaca {jumlah_buku_dibaca} buku')
+
+while jumlah_buku_dibaca < jumlah_buku:
+    jumlah_buku_dibaca = jumlah_buku_dibaca + 1
+    print(f'buku ke{jumlah_buku_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca  {jumlah_buku_dibaca}')
