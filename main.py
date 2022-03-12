bf63d5ad8595b48097b94f4dc3b0d96ce8f017af
WildanAruman <wildan.aruman90@gmail.com>
2022-03-12 11:58:12 +0700

    percabangan dasar python

---
+++ main.py
@@ -0,0 +1,21 @@
+# Sekuensial
+print('Ibu berkata , "pergi ke toko "')
+print('Budi menjawab, "Baik apa yang harus saya lakukan ditoko"')
+print('Ibu menjawab, Beli satu botol susu"')
+print('Maka budi berangkat ke toko')
+print('Dan mulai berbelanja')
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print("Budi beli 1 botol susu")
+else:
+    print('Budi tidak jadi beli 1 botol susu')
+
+
+
+
+
+
+
